fbe575ae52d8cc97e3bc35d005febd36ba3873fa
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-12 19:29:00 +0545

    entity user and course added

---
+++ src/main/java/com/onlinelearningplatform/Online/Learning/Platform/Entity/Course.java
@@ -0,0 +1,26 @@
+package com.onlinelearningplatform.Online.Learning.Platform.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private Long instructor_id;
+    private String created_at;
+}
