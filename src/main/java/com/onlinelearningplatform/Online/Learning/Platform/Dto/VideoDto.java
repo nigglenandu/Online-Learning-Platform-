2a51a299ff9e46578ca8b2a715b3c89183a7b807
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-20 16:36:17 +0545

    video service implemented

---
+++ src/main/java/com/onlinelearningplatform/Online/Learning/Platform/Dto/VideoDto.java
@@ -0,0 +1,22 @@
+package com.onlinelearningplatform.Online.Learning.Platform.Entity;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoDto {
+
+    @NotNull(message = "Title is required")
+    private String title;
+
+    @NotNull(message = "Video URL is required")
+    private String videoUrl;
+
+    @NotNull(message = "Create at is required")
+    private String createdAt;
+
+}
--- src/main/java/com/onlinelearningplatform/Online/Learning/Platform/Entity/VideoDto.java
+++
@@ -1,2 +0,0 @@
-package com.onlinelearningplatform.Online.Learning.Platform.Entity;public class VideoDtp {
-}
