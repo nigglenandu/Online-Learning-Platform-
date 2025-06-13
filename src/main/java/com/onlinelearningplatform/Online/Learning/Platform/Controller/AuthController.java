27452c58f50593c175b38225f8b8e5cf4d519ca1
[Niggle Nandu] <nigglenandu@gmail.com>
2025-06-13 23:02:22 +0545

    auth controller

--- src/main/java/com/onlinelearningplatform/Online/Learning/Platform/Controller/AuthController.java
+++ src/main/java/com/onlinelearningplatform/Online/Learning/Platform/Controller/AuthController.java
@@ -4,28 +4,32 @@ import com.onlinelearningplatform.Online.Learning.Platform.Dto.LoginDto;
 import com.onlinelearningplatform.Online.Learning.Platform.Entity.User;
 import com.onlinelearningplatform.Online.Learning.Platform.Dto.UserDto;
 import com.onlinelearningplatform.Online.Learning.Platform.Services.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class AuthController {
-    @Autowired
-    private IUserService userService;
+    private final IUserService userService;
+
+    public AuthController(IUserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody UserDto userDto){
+    public ResponseEntity<User> registerUser(@Valid @RequestBody UserDto userDto) {
         User user = userService.registerUser(userDto);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> authenticateUser(@RequestBody LoginDto loginDto){
+    public ResponseEntity<User> authenticateUser(@Valid @RequestBody LoginDto loginDto) {
         return userService.authenticateUser(loginDto)
-                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 }
